ca40236936746a86d453109e43864d9b4c4946bd
kinanen <otso.kinanen@gmail.com>
2023-05-18 14:56:17 +0200

    lisätty haku ja sivuttava totetuts AllStations näkymään

--- Solita-CityBikes/ClientApp/src/components/AllStations.js
+++ Solita-CityBikes/ClientApp/src/components/AllStations.js
@@ -1,81 +1,118 @@
-
-import React from "react";
-import { useTable } from "react-table";
+import React, { useState, useMemo } from "react";
+import { useTable, usePagination } from "react-table";
 
 const AllStations = ({ stationList, setStation, setTrip }) => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(25);
+  const [searchQuery, setSearchQuery] = useState('');
 
-    const data = React.useMemo(() => stationList);
+  const handleStationClick = (arg) => {
+    setTrip(null);
+    setStation(arg);
+  }
 
-    const handleStationClick = (arg) => {
-        setTrip(null);
-        setStation(arg);
-
-    }
-
-    const columns = React.useMemo(
-        () => [
-            {
-                Header: 'Kaikki asemat',
-                columns: [
-                    {
-                        Header: 'Aseman nimi',
-                        accessor: 'nimi',
-                        Cell: ({ cell }) => (
-                            <div onClick={() => handleStationClick(cell.row.original.hslStationId)}>
-                                {cell.value}
-                            </div>
-                        ),
-                    },
-                    {
-                        Header: 'Osoite',
-                        accessor: 'osoite',
-                    }
-                ],
-            },
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Kaikki asemat',
+        columns: [
+          {
+            Header: 'Aseman nimi',
+            accessor: 'nimi',
+            Cell: ({ cell }) => (
+              <div onClick={() => handleStationClick(cell.row.original.hslStationId)}>
+                {cell.value}
+              </div>
+            ),
+          },
+          {
+            Header: 'Osoite',
+            accessor: 'osoite',
+          }
         ],
-        []
-    )
+      },
+    ],
+    []
+  )
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable({
-        columns,
-        data,
-    })
+  const data = useMemo(() => stationList, [stationList]);
+  
+  const filteredData = useMemo(() => {
+    return data.filter(item =>
+      item.nimi.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [data, searchQuery]);
+  
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    nextPage,
+    previousPage,
+  } = useTable(
+    {
+      columns,
+      data : filteredData,
+      initialState: { pageIndex, pageSize },
+    },
+    usePagination
+  );
 
-    return (
-        <div className="list">
-            <table {...getTableProps()}>
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row, i) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
+  const handleSearch = event => {
+    setSearchQuery(event.target.value);
+    setPageIndex(0);
+  };
 
-    )
+
+
+  return (
+    <div className="list">
+      <div className="search-bar">
+        <input type="text" value={searchQuery} onChange={handleSearch} placeholder="Search" />
+      </div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={() => { previousPage(); setPageIndex(pageIndex - 1) }} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {Math.ceil(filteredData.length / pageSize)}
+          </strong>
+        </span>
+        <button onClick={() => { nextPage(); setPageIndex(pageIndex + 1) }} disabled={!canNextPage}>
+          Next
+        </button>
+      </div>
+    </div>
+  )
 }
-
 
 export default AllStations;
