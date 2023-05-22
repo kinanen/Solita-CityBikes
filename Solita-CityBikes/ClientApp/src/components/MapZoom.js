b145e0d6e8e529f6d5f862e148db47335db7e4ad
kinanen <otso.kinanen@gmail.com>
2023-05-22 21:42:06 +0200

    TripDetails komponentin ja sen Api:en koodausta

--- Solita-CityBikes/ClientApp/src/components/MapZoom.js
+++ Solita-CityBikes/ClientApp/src/components/MapZoom.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import {useMap} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
-import axios from "axios";
+import Stations from "../services/Stations";
 import * as L from "leaflet";
 
 
@@ -12,19 +12,17 @@ const MapZoom = ({trip, station}) => {
         const fetchData = async () => {
           try {
             if (station != null) {
-              const response = await axios.get(
-                `https://localhost:7199/api/station/${station}`
-              );
-              map.setView([response.data.y, response.data.x], 15);
-              map.openPopup(response.data.nimi, [response.data.y, response.data.x]);
+              Stations.getStation(station)
+              .then(response => {
+                map.setView([response.data.y, response.data.x], 15);
+                map.openPopup(response.data.nimi, [response.data.y, response.data.x]);
+              })
+             
             }
             if (trip != null) {
-                const response1 = await axios.get(
-                    `https://localhost:7199/api/station/${trip[0]}`
-                  );
-                const response2 = await axios.get(
-                    `https://localhost:7199/api/station/${trip[1]}`
-                );
+                const response1 = await Stations.getStation(trip[0]);
+                const response2 = await Stations.getStation(trip[1]);
+                
                   map.fitBounds([[response1.data.y, response1.data.x], [response2.data.y, response2.data.x]]);
                   map.addLayer(L.polyline([[response1.data.y, response1.data.x], [response2.data.y, response2.data.x]]))
                   map.openPopup(response1.data.nimi +" - "+response2.data.nimi, [(response1.data.y+response2.data.y)/2, (response1.data.x + response2.data.x)/2]);
