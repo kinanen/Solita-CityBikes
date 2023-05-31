656770d1322aea18e8cb7ce604853edcf07e5dce
kinanen <otso.kinanen@gmail.com>
2023-05-31 08:03:46 +0200

    korjattu Station ja Trip luokan testiluokkia

--- Solita-CityBikes-tests/StationTest.cs
+++ Solita-CityBikes-tests/StationTest.cs
@@ -1,22 +1,27 @@
-﻿using System;
-namespace Solita_CityBikes_
-{
-	public class StationTest
-	{
-		public StationTest()
-		{
-            /*
-            internal bool ValidateStationData()
-            {
-                if (this.X > 25.500 || this.X < 24.000) return false;
-                if (this.Y > 59.000 || this.Y < 60.500) return false;
-                if (Nimi == null) return false;
-                if (Osoite == null || Osoite.Length < 1) return false;
-                return true;
-            }
-            */
+﻿using Solita_CityBikes;
+using Solita_CityBikes.Controllers;
+using Solita_CityBikes.Data;
+using System.Reflection.Metadata;
 
+
+namespace Solita_CityBikes_;
+
+public class StationTest
+{
+    public StationTest()
+    {
+        /*
+        internal bool ValidateStationData()
+        {
+            if (this.X > 25.500 || this.X < 24.000) return false;
+            if (this.Y > 59.000 || this.Y < 60.500) return false;
+            if (Nimi == null) return false;
+            if (Osoite == null || Osoite.Length < 1) return false;
+            return true;
         }
-	}
+        */
+
+    }
 }
+
 
--- Solita-CityBikes-tests/TripTest.cs
+++ Solita-CityBikes-tests/TripTest.cs
@@ -1,23 +1,28 @@
-﻿using System;
-namespace Solita_CityBikes_
+﻿using Solita_CityBikes;
+using Solita_CityBikes.Controllers;
+using Solita_CityBikes.Data;
+using System.Reflection.Metadata;
+
+
+namespace Solita_CityBikes_;
+
+public class TripTest
 {
-	public class TripTest
-	{
-		public TripTest()
-		{
+    public TripTest()
+    {
 
-            /* bool ValidateTripData()
-            
+        /* bool ValidateTripData()
 
-                if (this.CoveredDistance < 10) return false;
-                if (this.Duration < 10) return false;
-                TimeSpan difference = new TimeSpan();
-                difference = this.ReturnTime.Subtract(this.DepartureTime);
-                if (difference.TotalSeconds < 10) return false;
-                return true;
 
-            */
-            
-        }
+            if (this.CoveredDistance < 10) return false;
+            if (this.Duration < 10) return false;
+            TimeSpan difference = new TimeSpan();
+            difference = this.ReturnTime.Subtract(this.DepartureTime);
+            if (difference.TotalSeconds < 10) return false;
+            return true;
+
+        */
+
+    }
 }
 
